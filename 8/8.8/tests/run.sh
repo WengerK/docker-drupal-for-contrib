433da0ee57f1725e1d702607a6368a0e770838e3
Kevin Wenger <wenger.kev@gmail.com>
2020-06-26 13:28:38 +0200

    add drupal 8.8.9

---
+++ 8/8.8/tests/run.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -e
+
+if [[ -n "${DEBUG}" ]]; then
+    set -x
+fi
+
+echo -n "Running tests for ${IMAGE}..."
+
+cid="$(
+	docker run -d "${IMAGE}"
+)"
+trap "docker rm -vf ${cid} > /dev/null" EXIT
+
+docker exec "${cid}" drush status | grep -q 'Drupal root    : /var/www/html'
+docker exec "${cid}" drush status | grep -q 'Drupal version : 8.8.8'
+docker exec "${cid}" drush status | grep -q 'Drush version  : 10.3.0'
+echo "OK"
