39ad33aa82d38e746cbba3fa648672511d8bea7f
Kevin Wenger <wenger.kev@gmail.com>
2020-06-26 16:07:38 +0200

    add the update.sh script to build/publish images

---
+++ update.sh
@@ -0,0 +1,93 @@
+#!/usr/bin/env bash
+set -e
+
+VERSION_TO_UPDATE="all"
+D8_LAST_VERSION=$(find ./8/* -maxdepth 1 -prune -type d -exec basename {} \; | sort -n | tail -n 1)
+D9_LAST_VERSION=$(find ./9/* -maxdepth 1 -prune -type d -exec basename {} \; | sort -n | tail -n 1)
+
+# Options
+while [ $# -gt 0 ]; do
+  case "$1" in
+    -h|--help)
+       echo "update.sh - update Dockerfile and scripts for all versions"
+       echo " "
+       echo "update.sh [options]"
+       echo " "
+       echo "options:"
+       echo "-h, --help                show brief help"
+       echo "-b, --build=VERSION       VERSION is optional, all images are build by default; set a VERSION like '7.2-node8'"
+       echo "--publish=VERSION         set a VERSION like '7.2-node8' or 'all'; publish also build images"
+       echo "--clean                   clean all local tags of the image"
+       exit 0
+       ;;
+    -b|--build*)
+      VERSION_TO_BUILD="${1#*=}"
+      if [ "$1" = "--build" ] || [ "$1" = "-b" ]; then
+        VERSION_TO_BUILD="all"
+      fi
+      VERSION_TO_UPDATE=$VERSION_TO_BUILD
+      ;;
+    --publish=*)
+      VERSION_TO_PUBLISH="${1#*=}"
+      if [ -z "$VERSION_TO_PUBLISH" ]; then
+        echo "VERSION to publish must be set"
+        exit 1
+      fi
+      VERSION_TO_BUILD=$VERSION_TO_PUBLISH
+      VERSION_TO_UPDATE=$VERSION_TO_PUBLISH
+      ;;
+    --clean)
+      if [ ! -z "$(docker images | grep wengerk/drupal-for-contrib | tr -s ' ' | cut -d ' ' -f 2 | grep -v '<none>')" ]; then
+        docker images | grep wengerk/drupal-for-contrib | tr -s ' ' | cut -d ' ' -f 2 | grep -v '<none>' | xargs -I {} docker rmi wengerk/drupal-for-contrib:{}
+      fi
+      echo "** images cleanded"
+      exit 0
+      ;;
+  esac
+  shift
+done
+
+# functions
+
+function build {
+  (
+    set -e
+    TAG=$2
+
+    echo "** build wengerk/drupal-for-contrib:$TAG"
+    cd ./$1/$2/
+    [[ -f ./Makefile ]] && make
+  )
+}
+
+function publish {
+  (
+    set -e
+    TAG=$2
+
+    echo "** publish wengerk/drupal-for-contrib:$TAG"
+    docker push wengerk/drupal-for-contrib:$TAG
+  )
+}
+
+function process {
+  drupalMajorVersion=$1
+  drupalVersion=$2
+
+  # build docker image(s) if required.
+  if [ "$VERSION_TO_BUILD" = "all" ] || [ "$VERSION_TO_BUILD" = "$drupalVersion" ]; then
+      build $drupalMajorVersion $drupalVersion
+  fi
+
+  # publish docker image(s) if required.
+  if [ "$VERSION_TO_PUBLISH" = "all" ] || [ "$VERSION_TO_PUBLISH" = "$drupalVersion" ]; then
+    publish $drupalMajorVersion $drupalVersion
+  fi
+}
+
+# Go through versions.
+for drupalMajorVersion in `find ./* -maxdepth 1 -prune -type d -exec basename {} \; | sort -n`; do
+  for drupalVersion in `find ./$drupalMajorVersion/* -maxdepth 1 -prune -type d -exec basename {} \; | sort -n`; do
+    process $drupalMajorVersion $drupalVersion
+  done
+done
