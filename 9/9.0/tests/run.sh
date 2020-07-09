875261d1b518b202f53af11e35e86dc8c4553f5a
Kevin Wenger <wenger.kev@gmail.com>
2020-07-09 15:55:19 +0200

    make tests more robust for 9.0.x instead of 9.0.1 hardcoded

--- 9/9.0/tests/run.sh
+++ 9/9.0/tests/run.sh
@@ -13,7 +13,7 @@ cid="$(
 trap "docker rm -vf ${cid} > /dev/null" EXIT
 
 docker exec "${cid}" drush status | grep -q 'Drupal root    : /var/www/html'
-docker exec "${cid}" drush status | grep -q 'Drupal version : 9.0.1'
+docker exec "${cid}" drush status | grep -q 'Drupal version : 9.0.'
 docker exec "${cid}" drush status | grep -q 'Drush version  : 10.'
 docker exec "${cid}" which phpunit | grep -q '/var/www/html/vendor/bin/phpunit'
 docker exec "${cid}" phpunit --version | grep -q 'PHPUnit 8.5.8 by Sebastian Bergmann and contributors.'
